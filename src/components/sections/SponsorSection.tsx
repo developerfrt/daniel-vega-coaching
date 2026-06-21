45b15268a78173274a44bce7b40c3b6e2dc94778
Farit Castillo <castillo.farit.18@gmail.com>
2026-06-21 01:23:41 -0500

    Restore sponsor dimensions and straighten static lower border

--- src/components/sections/SponsorSection.tsx
+++ src/components/sections/SponsorSection.tsx
@@ -1,4 +1,4 @@
-import sponsorImage from '../../assets/images/sponsor/xfit-mercado-libre-cropped.png'
+import sponsorImage from '../../assets/images/sponsor/xfit-mercado-libre-straight.png'
 import { Reveal } from '../common/Reveal'
 
 const MERCADO_LIBRE_URL =
