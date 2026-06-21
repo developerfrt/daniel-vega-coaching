17496383fbba078b4e87794db1980a7bee3eb22e
Farit Castillo <castillo.farit.18@gmail.com>
2026-06-21 00:37:30 -0500

    Force solid black sponsor banner lower strip

--- src/components/sections/SponsorSection.tsx
+++ src/components/sections/SponsorSection.tsx
@@ -1,4 +1,4 @@
-import sponsorImage from '../../assets/images/sponsor/xfit-mercado-libre.png'
+import sponsorImage from '../../assets/images/sponsor/xfit-mercado-libre-black.png'
 import { Reveal } from '../common/Reveal'
 
 const MERCADO_LIBRE_URL =
